e4aa5a6bd19aa516bf75eba7d962c6662a4fa33d
Thiago Moreira de Souza Arrais <thiagomds.scientist@gmail.com>
2020-11-02 13:41:45 -0300

    Create IClienteRepository and IClienteServices interfaces

---
+++ src/TMSA.PIZZARIA.Cadastro.Domain/Clientes/Interfaces/IClienteRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TMSA.PIZZARIA.Cadastro.Domain.Clientes.Interfaces
+{
+    public interface IClienteRepository
+    {
+        void CadastrarCliente(Cliente cliente);
+        IEnumerable<Cliente> ObterClientes();
+        Cliente ObterClientePorId(Guid idCliente);
+        Cliente ObterClientePorTelefone(string telefone);
+        void AtualizarCliente(Cliente cliente);
+        void RemoverCliente(Guid idCliente);
+    }
+}
