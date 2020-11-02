72c1daf6b0199187489877114aa7dd7b74c74869
Thiago Moreira de Souza Arrais <thiagomds.scientist@gmail.com>
2020-11-02 13:42:39 -0300

    Create ClienteServices class

---
+++ src/TMSA.PIZZARIA.Cadastro.Domain/Clientes/Services/ClienteServices.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using TMSA.PIZZARIA.Cadastro.Domain.Clientes.Interfaces;
+
+namespace TMSA.PIZZARIA.Cadastro.Domain.Clientes.Services
+{
+    public class ClienteServices : IClienteServices
+    {
+        private readonly IClienteRepository _clienteRepository;
+
+        public ClienteServices(IClienteRepository clienteRepository)
+        {
+            _clienteRepository = clienteRepository;
+        }
+
+        public void AtualizarCliente(Cliente cliente)
+        {
+            _clienteRepository.AtualizarCliente(cliente);
+        }
+
+        public void CadastrarCliente(Cliente cliente)
+        {
+            _clienteRepository.CadastrarCliente(cliente);
+        }
+
+        public Cliente ObterClientePorId(Guid idCliente)
+        {
+            return _clienteRepository.ObterClientePorId(idCliente);
+        }
+
+        public Cliente ObterClientePorTelefone(string telefone)
+        {
+            return _clienteRepository.ObterClientePorTelefone(telefone);
+        }
+
+        public IEnumerable<Cliente> ObterClientes()
+        {
+            return _clienteRepository.ObterClientes();
+        }
+
+        public void RemoverCliente(Guid idCliente)
+        {
+            _clienteRepository.RemoverCliente(idCliente);
+        }
+    }
+}
