1fa63496e69a4c18e8f9059678b92f6011e0bf5b
Thiago Moreira de Souza Arrais <thiagomds.scientist@gmail.com>
2020-11-02 15:01:28 -0300

    Create libray class TMSA.PIZZARIA.Core.Data to create Connection Data Base

---
+++ src/TMSA.PIZZARIA.Core.Data/Connection/ConnectionDB.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace TMSA.PIZZARIA.Core.Data.Connection
+{
+    public abstract class ConnectionDB
+    {
+        private readonly IConfiguration _configuration;
+
+        protected ConnectionDB(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public IDbConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_configuration.GetConnectionString("ConnectionDbApi"));
+            }
+        }
+    }
+}
