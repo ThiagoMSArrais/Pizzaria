9d2c01870a439b3b437c7e6af60ca2d62a896d55
Thiago Moreira de Souza Arrais <thiagomds.scientist@gmail.com>
2020-11-02 14:03:04 -0300

    Create class library TMSA.PIZZARIA.Core.CrossCutting.IoC and create class with name NativeInjectorBootStrapper

---
+++ src/TMSA.PIZZARIA.Core.Infra.CrossCutting.IoC/NativeInjectorBootStrapper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using TMSA.PIZZARIA.Cadastro.Data.Repository;
+using TMSA.PIZZARIA.Cadastro.Domain.Clientes.Interfaces;
+using TMSA.PIZZARIA.Cadastro.Domain.Clientes.Services;
+
+namespace TMSA.PIZZARIA.Core.Infra.CrossCutting.IoC
+{
+    public class NativeInjectorBootStrapper
+    {
+        public static void RegisterServices(IServiceCollection service)
+        {
+            service.AddScoped<IClienteServices, ClienteServices>();
+
+            service.AddScoped(IClienteRepository, ClienteRepository)
+        }
+    }
+}
