df737907ceebb2f101071b9494b87f7a91c0427a
Thiago Moreira de Souza Arrais <thiagomds.scientist@gmail.com>
2020-11-02 18:14:06 -0300

    Create API to Pizzaria, Create AutoMapperConfiguration and configurated Services at StartUp.cs

---
+++ src/TMSA.PIZZARIA.Api/DTO/ClienteDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TMSA.PIZZARIA.Api.DTO
+{
+    public class ClienteDto
+    {
+        public Guid IdCliente { get; set; }
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+        public EnderecoDto Endereco { get; set; }
+    }
+}
---
+++ src/TMSA.PIZZARIA.Api/DTO/EnderecoDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TMSA.PIZZARIA.Api.DTO
+{
+    public class EnderecoDto
+    {
+        public Guid IdEndereco { get; set; }
+        public string Logradouro { get; set; }
+        public string Numero { get; set; }
+        public string Complemento { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+        public string Cep { get; set; }
+    }
+}
