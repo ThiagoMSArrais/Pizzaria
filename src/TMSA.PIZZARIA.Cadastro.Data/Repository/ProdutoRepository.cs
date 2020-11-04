bb71f89b6e6dafc1e70704ed063049bf2d58dcfb
Thiago Moreira de Souza Arrais <thiagomds.scientist@gmail.com>
2020-11-04 10:37:47 -0300

    Create ProdutoRepository

---
+++ src/TMSA.PIZZARIA.Cadastro.Data/Repository/ProdutoRepository.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using TMSA.PIZZARIA.Cadastro.Domain.Produtos;
+using TMSA.PIZZARIA.Cadastro.Domain.Produtos.Interfaces;
+using TMSA.PIZZARIA.Core.Data.Connection;
+
+namespace TMSA.PIZZARIA.Cadastro.Data.Repository
+{
+    public class ProdutoRepository : ConnectionDB, IProdutoRepository
+    {
+        public ProdutoRepository(IConfiguration configuration) : base(configuration)
+        {
+        }
+
+        public void AtualizarCategoria(Categoria categoria)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AtualizarProduto(Produto prodto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CadastrarCategoria(Categoria categoria)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CadastrarProduto(Produto produto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Categoria ObterCategoriaPorId(Guid idCategoria)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Categoria> ObterCategorias()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Produto ObterprodutoPorCategoria(Guid idCategoria)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Produto ObterProdutoPorId(Guid idProduto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Produto> ObterProdutos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoverCategoria(Guid idCategoria)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoverProduto(Guid idProduto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
