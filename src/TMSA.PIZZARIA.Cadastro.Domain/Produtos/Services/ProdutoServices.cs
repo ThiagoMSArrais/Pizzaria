75abda20b533dcc4e453b6c20f99c4f8120560a0
Thiago Moreira de Souza Arrais <thiagomds.scientist@gmail.com>
2020-11-04 10:37:02 -0300

    Create class domain service of ProdutoServices

---
+++ src/TMSA.PIZZARIA.Cadastro.Domain/Produtos/Services/ProdutoServices.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using TMSA.PIZZARIA.Cadastro.Domain.Produtos.Interfaces;
+
+namespace TMSA.PIZZARIA.Cadastro.Domain.Produtos.Services
+{
+    public class ProdutoServices : IProdutoServices
+    {
+        public void AtualizarCategoria(Categoria categoria)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AtualizarProduto(Produto prodto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CadastrarCategoria(Categoria categoria)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CadastrarProduto(Produto produto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Categoria ObterCategoriaPorId(Guid idCategoria)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Categoria> ObterCategorias()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Produto ObterprodutoPorCategoria(Guid idCategoria)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Produto ObterProdutoPorId(Guid idProduto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Produto> ObterProdutos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoverCategoria(Guid idCategoria)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoverProduto(Guid idProduto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
