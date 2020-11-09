5f619957e0907db56498c1b39456a86f864f695f
Thiago Moreira de Souza Arrais <thiagomds.scientist@gmail.com>
2020-11-09 13:06:18 -0300

    .

--- src/TMSA.PIZZARIA.Cadastro.Data/Repository/ProdutoRepository.cs
+++ src/TMSA.PIZZARIA.Cadastro.Data/Repository/ProdutoRepository.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using TMSA.PIZZARIA.Cadastro.Domain.Produtos;
 using TMSA.PIZZARIA.Cadastro.Domain.Produtos.Interfaces;
 using TMSA.PIZZARIA.Core.Data.Connection;
@@ -15,7 +17,34 @@ namespace TMSA.PIZZARIA.Cadastro.Data.Repository
 
         public void AtualizarCategoria(Categoria categoria)
         {
-            throw new NotImplementedException();
+            using(IDbConnection con = Connection)
+            {
+                string sqlAtualizarCategoria = @"UPDATE T_CATEGORIA 
+                                                 SET DS_CATEGORIA = @CATEGORIA
+                                                 WHERE
+                                                        ID_CATEGORIA = @IDCATEGORIA";
+
+                if (con.State == ConnectionState.Closed)
+                    con.Open();
+
+                DynamicParameters parametros = new DynamicParameters();
+                parametros.Add("CATEGORIA", categoria.Tipo, DbType.String, ParameterDirection.Input);
+                parametros.Add("IDCATEGORIA", categoria.IdCategoria, DbType.Guid, ParameterDirection.Input);
+
+                try
+                {
+                    con.Execute(sql: sqlAtualizarCategoria, param: parametros);
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+                finally
+                {
+                    con.Close();
+                    con.Dispose();
+                }
+            }
         }
 
         public void AtualizarProduto(Produto prodto)
@@ -60,12 +89,58 @@ namespace TMSA.PIZZARIA.Cadastro.Data.Repository
 
         public void RemoverCategoria(Guid idCategoria)
         {
-            throw new NotImplementedException();
+            using (IDbConnection con = Connection)
+            {
+                string sqlRemoverCategoria = @"DELETE T_CATEGORIA WHERE ID_CATEGORIA = @IDCATEGORIA";
+
+                if (con.State == ConnectionState.Closed)
+                    con.Open();
+
+                DynamicParameters parametros = new DynamicParameters();
+                parametros.Add("IDCATEGORIA", idCategoria, DbType.Guid, ParameterDirection.Input);
+
+                try
+                {
+                    con.Execute(sql: sqlRemoverCategoria, param: parametros);
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+                finally
+                {
+                    con.Close();
+                    con.Dispose();
+                }
+            }
         }
 
         public void RemoverProduto(Guid idProduto)
         {
-            throw new NotImplementedException();
+            using (IDbConnection con = Connection)
+            {
+                string sqlRemoverProduto = @"DELETE T_PRODUTO WHERE ID_PRODUTO = @IDPRODUTO";
+
+                if (con.State == ConnectionState.Closed)
+                    con.Open();
+
+                DynamicParameters parametros = new DynamicParameters();
+                parametros.Add("IDPRODUTO", idProduto, DbType.Guid, ParameterDirection.Input);
+
+                try
+                {
+                    con.Execute(sql: sqlRemoverProduto, param: parametros);
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+                finally
+                {
+                    con.Close();
+                    con.Dispose();
+                }
+            }
         }
     }
 }
