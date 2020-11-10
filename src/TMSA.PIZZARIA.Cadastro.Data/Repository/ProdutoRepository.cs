b8a6f2843e02800c9a4f411a49837bb132702b4a
Thiago Moreira de Souza Arrais <thiagomds.scientist@gmail.com>
2020-11-10 09:00:39 -0300

    Implement ObterProdutoPorid, ObterprodutoPorCategoria, ObterProdutos, CadastrarProduto

--- src/TMSA.PIZZARIA.Cadastro.Data/Repository/ProdutoRepository.cs
+++ src/TMSA.PIZZARIA.Cadastro.Data/Repository/ProdutoRepository.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Linq;
 using TMSA.PIZZARIA.Cadastro.Domain.Produtos;
 using TMSA.PIZZARIA.Cadastro.Domain.Produtos.Interfaces;
 using TMSA.PIZZARIA.Core.Data.Connection;
@@ -54,37 +55,294 @@ namespace TMSA.PIZZARIA.Cadastro.Data.Repository
 
         public void CadastrarCategoria(Categoria categoria)
         {
+            //string sqlCadastrarCategoria = $"INSERT INTO T_CATEGORIA" +
+            //                                   $"(" +
+            //                                       $"ID_CATEGORIA," +
+            //                                       $"DS_CATEGORIA " +
+            //                                   $")" +
+            //                                   $"VALUES " +
+            //                                   $"( " +
+            //                                       $"@ID_CATEGORIA," +
+            //                                       $"@DS_CATEGORIA " +
+            //                                   $")";
             throw new NotImplementedException();
         }
 
         public void CadastrarProduto(Produto produto)
         {
-            throw new NotImplementedException();
-        }
+            using (IDbConnection con = Connection)
+            {
+                string sqlCadastrarProduto = $"INSERT INTO T_PRODUTO " +
+                                             $"(" +
+                                                 $"ID_PRODUTO," +
+                                                 $"NM_PRODUTO," +
+                                                 $"{(!string.IsNullOrEmpty(produto.Descricao) ? "DS_PRODUTO, " : string.Empty)}" +
+                                                 $"NU_QTD_PRODUTO," +
+                                                 $"CD_CATEGORIA" +
+                                             $") " +
+                                             $"VALUES " +
+                                             $"(" +
+                                                 $"@ID_PRODUTO," +
+                                                 $"@NM_PRODUTO," +
+                                                 $"{(!string.IsNullOrEmpty(produto.Descricao) ? "@DS_PRODUTO, " : string.Empty)}" +
+                                                 $"@NU_QTD_PRODUTO," +
+                                                 $"@CD_CATEGORIA" +
+                                             $") ";
 
+                
+                DynamicParameters parametros = new DynamicParameters();
+
+                parametros.Add("ID_PRODUTO", produto.IdProduto, DbType.Guid, ParameterDirection.Input);
+                parametros.Add("NM_PRODUTO", produto.Nome, DbType.String, ParameterDirection.Input);
+                parametros.Add("NU_QTD_PRODUTO", produto.Quantidade, DbType.Int32, ParameterDirection.Input);
+                parametros.Add("CD_CATEGORIA", produto.Categoria.IdCategoria, DbType.Guid, ParameterDirection.Input);
+                
+                if (!string.IsNullOrEmpty(produto.Descricao))
+                    parametros.Add("DS_PRODUTO", produto.Descricao, DbType.String, ParameterDirection.Input);
+
+                try
+                {
+                    if (con.State == ConnectionState.Closed)
+                        con.Open();
+
+                    con.Execute(sql: sqlCadastrarProduto, param: parametros);
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+                finally
+                {
+                    con.Close();
+                    con.Dispose();
+                }
+            }
+        }
         public Categoria ObterCategoriaPorId(Guid idCategoria)
         {
-            throw new NotImplementedException();
+            using (IDbConnection con = Connection)
+            {
+                string sqlObterCategoriaPorId = @"SELECT 
+                                                          CATE.ID_CATEGORIA AS IdCategoria,
+                                                          CATE.DS_CATEGORIA AS Tipo
+                                               
+                                                  FROM  
+                                                         T_CATEGORIA CATE
+                                                  WHERE
+                                                         CATE.ID_CATEGORIA = @IDCATEGORIA";
+
+                Categoria categoria = default(Categoria);
+
+                DynamicParameters parametros = new DynamicParameters();
+                parametros.Add("IDCATEGORIA", idCategoria, DbType.Guid, ParameterDirection.Input);
+
+                try
+                {
+                    if (con.State == ConnectionState.Closed)
+                        con.Open();
+
+                    categoria = con.QueryFirstOrDefault<Categoria>(sql: sqlObterCategorias, param: parametros);
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+                finally
+                {
+                    con.Close();
+                    con.Dispose();
+                }
+
+                return categoria;
+            }
         }
 
         public IEnumerable<Categoria> ObterCategorias()
         {
-            throw new NotImplementedException();
+            using (IDbConnection con = Connection)
+            {
+                string sqlObterCategorias = @"SELECT 
+                                                      CATE.ID_CATEGORIA AS IdCategoria,
+                                                      CATE.DS_CATEGORIA AS Tipo
+
+                                              FROM  
+                                                     T_CATEGORIA CATE";
+
+                IEnumerable<Categoria> categorias = default(IEnumerable<Categoria>);
+
+                try
+                {
+                    if (con.State == ConnectionState.Closed)
+                        con.Open();
+
+                    categorias = con.Query<Categoria>(sql: sqlObterCategorias);
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+                finally
+                {
+                    con.Close();
+                    con.Dispose();
+                }
+
+                return categorias;
+            }
         }
 
         public Produto ObterprodutoPorCategoria(Guid idCategoria)
         {
-            throw new NotImplementedException();
+            using (IDbConnection con = Connection)
+            {
+                string sqlObterProdutosPorId = @"SELECT 
+                                                         PROD.ID_PRODUTO AS IdProduto,
+                                                         PROD.NM_PRODUTO AS Nome,
+                                                         PROD.DS_PRODUTO AS Descricao,
+                                                         PROD.VL_PRECO AS Preco,
+                                                         PROD.NU_QTD_PRODUTO AS Quantidade,
+                                                         CATE.ID_CATEGORIA AS IdCategoria,
+                                                         CATE.DS_CATEGORIA AS Tipo
+                                                
+                                                  FROM  
+                                                          T_PRODUTO PROD
+                                                          JOIN T_CATEGORIA CATE 
+                                                          ON PROD.CD_CATEGORIA = CATE.ID_CATEGORIA
+                                                  WHERE
+                                                          PROD.CD_CATEGORIA = @IDCATEGORIA";
+
+                IEnumerable<Produto> produto = default(IEnumerable<Produto>);
+
+                DynamicParameters parametros = new DynamicParameters();
+                parametros.Add("IDCATEGORIA", idCategoria, DbType.Guid, ParameterDirection.Input);
+
+                try
+                {
+                    if (con.State == ConnectionState.Closed)
+                        con.Open();
+
+                    produto = con.Query<Produto, Categoria, Produto>(sql: sqlObterProdutosPorId, param: parametros,
+                                      map: (produto, categoria) =>
+                                      {
+                                          produto.Categoria = categoria;
+                                          return produto;
+                                      }, splitOn: "IdCategoria");
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+                finally
+                {
+                    con.Close();
+                    con.Dispose();
+                }
+
+                return produto.FirstOrDefault();
+            }
         }
 
         public Produto ObterProdutoPorId(Guid idProduto)
         {
-            throw new NotImplementedException();
+            using (IDbConnection con = Connection)
+            {
+                string sqlObterProdutosPorId = @"SELECT 
+                                                         PROD.ID_PRODUTO AS IdProduto,
+                                                         PROD.NM_PRODUTO AS Nome,
+                                                         PROD.DS_PRODUTO AS Descricao,
+                                                         PROD.VL_PRECO AS Preco,
+                                                         PROD.NU_QTD_PRODUTO AS Quantidade,
+                                                         CATE.ID_CATEGORIA AS IdCategoria,
+                                                         CATE.DS_CATEGORIA AS Tipo
+                                                
+                                                  FROM  
+                                                          T_PRODUTO PROD
+                                                          JOIN T_CATEGORIA CATE 
+                                                          ON PROD.CD_CATEGORIA = CATE.ID_CATEGORIA
+                                                  WHERE
+                                                          PROD.ID_PRODUTO = @IDPRODUTO";
+
+                IEnumerable<Produto> produto = default(IEnumerable<Produto>);
+
+                DynamicParameters parametros = new DynamicParameters();
+                parametros.Add("IDPRODUTO", idProduto, DbType.Guid, ParameterDirection.Input);
+
+                try
+                {
+                    if (con.State == ConnectionState.Closed)
+                        con.Open();
+
+                    produto = con.Query<Produto, Categoria, Produto>(sql: sqlObterProdutosPorId, param: parametros,
+                                      map: (produto, categoria) =>
+                                      {
+                                          produto.Categoria = categoria;
+                                          return produto;
+                                      }, splitOn: "IdCategoria");
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+                finally
+                {
+                    con.Close();
+                    con.Dispose();
+                }
+
+                return produto.FirstOrDefault();
+            }
         }
 
         public IEnumerable<Produto> ObterProdutos()
         {
-            throw new NotImplementedException();
+            using(IDbConnection con = Connection)
+            {
+                string sqlObterProdutos = @"SELECT 
+                                                    PROD.ID_PRODUTO AS IdProduto,
+                                                    PROD.NM_PRODUTO AS Nome,
+                                                    PROD.DS_PRODUTO AS Descricao,
+                                                    PROD.VL_PRECO AS Preco,
+                                                    PROD.NU_QTD_PRODUTO AS Quantidade,
+                                                    CATE.ID_CATEGORIA AS IdCategoria,
+                                                    CATE.DS_CATEGORIA AS Tipo
+
+                                             FROM  
+                                                     T_PRODUTO PROD
+                                                     JOIN T_CATEGORIA CATE 
+                                                     ON PROD.CD_CATEGORIA = CATE.ID_CATEGORIA";
+
+                IEnumerable<Produto> produtos = default(IEnumerable<Produto>);
+
+                try
+                {
+                    if (con.State == ConnectionState.Closed)
+                        con.Open();
+
+                    produtos = con.Query<Produto, Categoria, Produto>(sql: sqlObterProdutos,
+                                        map: (produto, categoria) =>
+                                        {
+                                            produto.Categoria = categoria;
+                                            return produto;
+                                        }, splitOn: "IdCategoria");
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+                finally
+                {
+                    con.Close();
+                    con.Dispose();
+                }
+
+                return produtos;
+            }
         }
 
         public void RemoverCategoria(Guid idCategoria)
